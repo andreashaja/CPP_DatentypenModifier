ee153781262f892d6f17b18c71bfe62a37fbac88
Andreas Haja <ah@andreashaja.com>
2020-12-04 09:52:22 +0100

    first set of exercises

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* ---------------------------------------------------------------------
+ * The Fearless Engineer - Programmieren und Elektronik
+ * Copyright (C) 2020, Dr. Andreas Haja.  
+ *
+ * Zweck : Übungsaufgaben zu elementaren Datentypen und Modifiern in C++
+ *
+ * Du solltest zusammen mit diesem Programm eine Kopie der MIT-Lizenz erhalten haben.
+ * Falls nicht, sieh bitte hier nach: https://opensource.org/licenses/MIT.
+ *
+ * Das Begleitheft zum Modul E2_M3 kann hier geladen werden:
+ * http://www.thefearlessengineer.com/cpp-kurs-e2
+ * ----------------------------------------------------------------------
+ */
+
